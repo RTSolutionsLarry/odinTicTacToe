2c6293427c7b8b9e92ca21a823da4572dc76ade3
RTSolutionsLarry <larry.randolph@randolphtechnologysolutions.com>
2025-11-06 19:05:46 -0800

    Create expandable grid

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
-const gameboard = (() => {
-    
+const gameboard = ( function () {
+    const gameBoardContainer = document.getElementsByClassName('gameBoardContainer')[0];
+
+    const createBoard = (size) => {
+        let gridSize = '';
+        for (let i = 0; i < size; i++) {
+            gridSize = gridSize + ' 1fr';
+            console.log(gridSize);
+        }
+        
+
+        gameBoardContainer.style.gridTemplateColumns = gridSize; 
+
+        for (let i = 0; i < size; i++) {
+            for (let j = 0; j < size; j++) {
+                const gameSquare = document.createElement('div');
+                gameSquare.classList.add('gameSquare');
+                gameBoardContainer.appendChild(gameSquare);
+            }
+
+        }
+    }
+    return {createBoard};
 })();
+
+gameboard.createBoard(3);
 
 const game = (() => {
 
